00112a3f99615afae122e7b577467ba0ffde8d8a
Sadwika11 <sadwika9024@gmail.com>
2025-11-17 23:52:48 +0530

    Add backend source code with Spring Boot configuration

---
+++ src/main/java/com/example/FocusFlow/FocusFlowApplication.java
@@ -0,0 +1,15 @@
+package com.example.FocusFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+public class FocusFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusFlowApplication.class, args);
+	}
+
+}
